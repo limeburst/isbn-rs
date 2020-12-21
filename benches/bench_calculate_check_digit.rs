cb7ff1a978a68333c1f57214a6b33cd5824db6cd
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-12-21 02:48:11 -0800

    Push local updates

---
+++ benches/bench_calculate_check_digit.rs
@@ -0,0 +1,27 @@
+#![feature(test)]
+extern crate test;
+use test::Bencher;
+
+use core::hint::black_box;
+use core::str::FromStr;
+use isbn::*;
+
+#[bench]
+fn bench_calculate_check_digit_10(b: &mut Bencher) {
+    b.iter(|| black_box(Isbn10::new(black_box([9, 9, 7, 1, 5, 0, 2, 1, 0, 0]))))
+}
+
+#[bench]
+fn bench_calculate_check_digit_13(b: &mut Bencher) {
+    b.iter(|| {
+        black_box(Isbn13::new(black_box([
+            9, 7, 8, 3, 1, 6, 1, 4, 8, 4, 1, 0, 0,
+        ])))
+    })
+}
+
+#[bench]
+fn bench_convert(b: &mut Bencher) {
+    let a = black_box(Isbn10::new([9, 9, 7, 1, 5, 0, 2, 1, 0, 0]).unwrap());
+    b.iter(|| black_box(Isbn13::from(a)))
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![no_std]
 //! A library for handling [International Standard Book Number], or ISBNs.
 //!
 //! # Examples
@@ -6,12 +5,12 @@
 //! ```
 //! use isbn::{Isbn10, Isbn13};
 //!
-//! let isbn_10 = Isbn10::new(8, 9, 6, 6, 2, 6, 1, 2, 6, 4).unwrap();
+//! let isbn_10 = Isbn10::new([8, 9, 6, 6, 2, 6, 1, 2, 6, 4]).unwrap();
 //! assert_eq!(isbn_10.hyphenate().unwrap().as_str(), "89-6626-126-4");
 //! assert_eq!(isbn_10.registration_group(), Ok("Korea, Republic"));
 //! assert_eq!("89-6626-126-4".parse(), Ok(isbn_10));
 //!
-//! let isbn_13 = Isbn13::new(9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5).unwrap();
+//! let isbn_13 = Isbn13::new([9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5]).unwrap();
 //! assert_eq!(isbn_13.hyphenate().unwrap().as_str(), "978-1-4920-6766-5");
 //! assert_eq!(isbn_13.registration_group(), Ok("English language"));
 //! assert_eq!("978-1-4920-6766-5".parse(), Ok(isbn_13));
@@ -24,7 +23,7 @@ use core::fmt;
 use core::num::ParseIntError;
 use core::str::FromStr;
 
-use arrayvec::{ArrayString, ArrayVec};
+use arrayvec::{ArrayString, ArrayVec, CapacityError};
 
 pub type IsbnResult<T> = Result<T, IsbnError>;
 
@@ -37,8 +36,8 @@ include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 /// ```
 /// use isbn::{Isbn, Isbn10, Isbn13};
 ///
-/// let isbn_10 = Isbn::_10(Isbn10::new(8, 9, 6, 6, 2, 6, 1, 2, 6, 4).unwrap());
-/// let isbn_13 = Isbn::_13(Isbn13::new(9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5).unwrap());
+/// let isbn_10 = Isbn::_10(Isbn10::new([8, 9, 6, 6, 2, 6, 1, 2, 6, 4]).unwrap());
+/// let isbn_13 = Isbn::_13(Isbn13::new([9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5]).unwrap());
 ///
 /// assert_eq!("89-6626-126-4".parse(), Ok(isbn_10));
 /// assert_eq!("978-1-4920-6766-5".parse(), Ok(isbn_13));
@@ -66,8 +65,8 @@ impl Isbn {
     /// ```
     /// use isbn::{Isbn, Isbn10, Isbn13};
     ///
-    /// let isbn_10 = Isbn::_10(Isbn10::new(8, 9, 6, 6, 2, 6, 1, 2, 6, 4).unwrap());
-    /// let isbn_13 = Isbn::_13(Isbn13::new(9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5).unwrap());
+    /// let isbn_10 = Isbn::_10(Isbn10::new([8, 9, 6, 6, 2, 6, 1, 2, 6, 4]).unwrap());
+    /// let isbn_13 = Isbn::_13(Isbn13::new([9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5]).unwrap());
     ///
     /// assert_eq!(isbn_10.hyphenate().unwrap().as_str(), "89-6626-126-4");
     /// assert_eq!(isbn_13.hyphenate().unwrap().as_str(), "978-1-4920-6766-5");
@@ -84,8 +83,8 @@ impl Isbn {
     /// ```
     /// use isbn::{Isbn, Isbn10, Isbn13};
     ///
-    /// let isbn_10 = Isbn::_10(Isbn10::new(8, 9, 6, 6, 2, 6, 1, 2, 6, 4).unwrap());
-    /// let isbn_13 = Isbn::_13(Isbn13::new(9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5).unwrap());
+    /// let isbn_10 = Isbn::_10(Isbn10::new([8, 9, 6, 6, 2, 6, 1, 2, 6, 4]).unwrap());
+    /// let isbn_13 = Isbn::_13(Isbn13::new([9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5]).unwrap());
     ///
     /// assert_eq!(isbn_10.registration_group(), Ok("Korea, Republic"));
     /// assert_eq!(isbn_13.registration_group(), Ok("English language"));
@@ -122,7 +121,7 @@ impl From<Isbn13> for Isbn {
 impl FromStr for Isbn {
     type Err = IsbnError;
     fn from_str(s: &str) -> Result<Isbn, IsbnError> {
-        Parser::new(s).read_isbn()
+        Parser::new(s)?.read_isbn()
     }
 }
 
@@ -152,22 +151,12 @@ impl Isbn10 {
     /// ```
     /// use isbn::Isbn10;
     ///
-    /// let isbn10 = Isbn10::new(8, 9, 6, 6, 2, 6, 1, 2, 6, 4).unwrap();
+    /// let isbn10 = Isbn10::new([8, 9, 6, 6, 2, 6, 1, 2, 6, 4]).unwrap();
     /// ```
-    pub fn new(
-        a: u8,
-        b: u8,
-        c: u8,
-        d: u8,
-        e: u8,
-        f: u8,
-        g: u8,
-        h: u8,
-        i: u8,
-        j: u8,
-    ) -> IsbnResult<Isbn10> {
-        let digits = [a, b, c, d, e, f, g, h, i, j];
-        if Isbn10::calculate_check_digit(&digits) == j {
+    pub fn new(digits: [u8; 10]) -> IsbnResult<Isbn10> {
+        if digits[..9].iter().any(|d| *d > 9) || digits[9] > 10 {
+            Err(IsbnError::DigitTooLarge)
+        } else if Isbn10::calculate_check_digit(&digits) == digits[9] {
             Ok(Isbn10 { digits })
         } else {
             Err(IsbnError::InvalidChecksum)
@@ -179,28 +168,32 @@ impl Isbn10 {
     /// ```
     /// use isbn::{Isbn10, Isbn13};
     ///
-    /// let isbn_13 = Isbn13::new(9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5).unwrap();
+    /// let isbn_13 = Isbn13::new([9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5]).unwrap();
     /// assert_eq!(Isbn10::try_from(isbn_13), "1-4920-6766-0".parse());
     /// ```
     pub fn try_from(isbn13: Isbn13) -> IsbnResult<Self> {
-        let d = isbn13.digits;
-        if d[..3] != [9, 7, 8] {
+        if isbn13.digits[..3] != [9, 7, 8] {
             Err(IsbnError::InvalidConversion)
         } else {
-            let c = Isbn10::calculate_check_digit(&isbn13.digits[3..]);
-            Isbn10::new(d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10], d[11], c)
+            let mut a = [0; 10];
+            a[..9].clone_from_slice(&isbn13.digits[3..12]);
+            a[9] = Isbn10::calculate_check_digit(&a);
+            Ok(Isbn10 { digits: a })
         }
     }
 
-    fn calculate_check_digit(digits: &[u8]) -> u8 {
-        let sum: usize = digits
+    fn calculate_check_digit(digits: &[u8; 10]) -> u8 {
+        let sum: usize = digits[..9]
             .iter()
             .enumerate()
-            .take(9)
             .map(|(i, &d)| d as usize * (10 - i))
             .sum();
-        let check_digit = (11 - (sum % 11)) % 11;
-        check_digit as u8
+        let sum_m = (sum % 11) as u8;
+        if sum_m == 0 {
+            0
+        } else {
+            11 - sum_m
+        }
     }
 
     /// Hyphenate an ISBN-10 into its parts:
@@ -213,7 +206,7 @@ impl Isbn10 {
     /// ```
     /// use isbn::Isbn10;
     ///
-    /// let isbn_10 = Isbn10::new(8, 9, 6, 6, 2, 6, 1, 2, 6, 4).unwrap();
+    /// let isbn_10 = Isbn10::new([8, 9, 6, 6, 2, 6, 1, 2, 6, 4]).unwrap();
     /// assert_eq!(isbn_10.hyphenate().unwrap().as_str(), "89-6626-126-4");
     /// ```
     pub fn hyphenate(&self) -> Result<ArrayString<[u8; 17]>, IsbnError> {
@@ -238,7 +231,7 @@ impl Isbn10 {
     /// ```
     /// use isbn::Isbn10;
     ///
-    /// let isbn_10 = Isbn10::new(8, 9, 6, 6, 2, 6, 1, 2, 6, 4).unwrap();
+    /// let isbn_10 = Isbn10::new([8, 9, 6, 6, 2, 6, 1, 2, 6, 4]).unwrap();
     /// assert_eq!(isbn_10.registration_group(), Ok("Korea, Republic"));
     /// ```
     pub fn registration_group(&self) -> Result<&str, IsbnError> {
@@ -265,20 +258,28 @@ impl Isbn10 {
 
 impl fmt::Display for Isbn10 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for x in &self.digits {
-            match x {
-                10 => write!(f, "X")?,
-                _ => write!(f, "{}", x)?,
+        fn convert(d: u8) -> char {
+            if d < 10 {
+                ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'][d as usize]
+            } else {
+                'X'
             }
         }
-        Ok(())
+        let mut s = ArrayString::<[u8; 10]>::new();
+        self.digits.iter().for_each(|&c| s.push(convert(c)));
+        write!(f, "{}", s)
     }
 }
 
 impl FromStr for Isbn10 {
     type Err = IsbnError;
     fn from_str(s: &str) -> Result<Isbn10, IsbnError> {
-        Parser::new(s).read_isbn10()
+        let mut p = Parser::new(s)?;
+        if p.digits.len() != 10 {
+            Err(IsbnError::InvalidLength)
+        } else {
+            p.read_isbn10()
+        }
     }
 }
 
@@ -296,40 +297,29 @@ impl Isbn13 {
     /// ```
     /// use isbn::Isbn13;
     ///
-    /// let isbn13 = Isbn13::new(9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5).unwrap();
+    /// let isbn13 = Isbn13::new([9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5]).unwrap();
     /// ```
-    pub fn new(
-        a: u8,
-        b: u8,
-        c: u8,
-        d: u8,
-        e: u8,
-        f: u8,
-        g: u8,
-        h: u8,
-        i: u8,
-        j: u8,
-        k: u8,
-        l: u8,
-        m: u8,
-    ) -> IsbnResult<Isbn13> {
-        let digits = [a, b, c, d, e, f, g, h, i, j, k, l, m];
-        if Isbn13::calculate_check_digit(&digits) == m {
+    pub fn new(digits: [u8; 13]) -> IsbnResult<Isbn13> {
+        if digits.iter().any(|d| *d > 9) {
+            Err(IsbnError::DigitTooLarge)
+        } else if Isbn13::calculate_check_digit(&digits) == digits[12] {
             Ok(Isbn13 { digits })
         } else {
             Err(IsbnError::InvalidChecksum)
         }
     }
 
-    fn calculate_check_digit(digits: &[u8]) -> u8 {
-        let sum: usize = digits
-            .iter()
-            .enumerate()
-            .take(12)
-            .map(|(i, &d)| d as usize * (3 - 2 * ((i + 1) % 2)))
-            .sum();
-        let check_digit = (10 - (sum % 10)) % 10;
-        check_digit as u8
+    fn calculate_check_digit(digits: &[u8; 13]) -> u8 {
+        let mut sum = 0;
+        for i in 0..6 {
+            sum += u16::from(digits[i * 2] + 3 * digits[i * 2 + 1]);
+        }
+        let sum_m = (sum % 10) as u8;
+        if sum_m == 0 {
+            0
+        } else {
+            10 - sum_m
+        }
     }
 
     fn ean_ucc_group(&self) -> Result<Group, IsbnError> {
@@ -351,7 +341,7 @@ impl Isbn13 {
     /// ```
     /// use isbn::Isbn13;
     ///
-    /// let isbn_13 = Isbn13::new(9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5).unwrap();
+    /// let isbn_13 = Isbn13::new([9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5]).unwrap();
     /// assert_eq!(isbn_13.hyphenate().unwrap().as_str(), "978-1-4920-6766-5");
     /// ```
     pub fn hyphenate(&self) -> Result<ArrayString<[u8; 17]>, IsbnError> {
@@ -376,7 +366,7 @@ impl Isbn13 {
     /// ```
     /// use isbn::Isbn13;
     ///
-    /// let isbn_13 = Isbn13::new(9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5).unwrap();
+    /// let isbn_13 = Isbn13::new([9, 7, 8, 1, 4, 9, 2, 0, 6, 7, 6, 6, 5]).unwrap();
     /// assert_eq!(isbn_13.registration_group(), Ok("English language"));
     /// ```
     pub fn registration_group(&self) -> Result<&str, IsbnError> {
@@ -402,31 +392,38 @@ impl Isbn13 {
 
 impl fmt::Display for Isbn13 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for x in &self.digits {
-            write!(f, "{}", x)?;
+        fn convert(d: u8) -> char {
+            if d < 10 {
+                ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'][d as usize]
+            } else {
+                'X'
+            }
         }
-        Ok(())
+        let mut s = ArrayString::<[u8; 13]>::new();
+        self.digits.iter().for_each(|&c| s.push(convert(c)));
+        write!(f, "{}", s)
     }
 }
 
 impl From<Isbn10> for Isbn13 {
     fn from(isbn10: Isbn10) -> Isbn13 {
-        let mut v = ArrayVec::<[u8; 13]>::new();
-        v.extend([9, 7, 8].iter().cloned());
-        v.extend(isbn10.digits[..9].iter().cloned());
-        let c = Isbn13::calculate_check_digit(&v);
-        let d = isbn10.digits;
-        Isbn13::new(
-            9, 7, 8, d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], c,
-        )
-        .unwrap()
+        let mut a = [0; 13];
+        a[..3].clone_from_slice(&[9, 7, 8]);
+        a[3..12].clone_from_slice(&isbn10.digits[0..9]);
+        a[12] = Isbn13::calculate_check_digit(&a);
+        Isbn13 { digits: a }
     }
 }
 
 impl FromStr for Isbn13 {
     type Err = IsbnError;
     fn from_str(s: &str) -> Result<Isbn13, IsbnError> {
-        Parser::new(s).read_isbn13()
+        let mut p = Parser::new(s)?;
+        if p.digits.len() != 13 {
+            Err(IsbnError::InvalidLength)
+        } else {
+            p.read_isbn13()
+        }
     }
 }
 
@@ -445,6 +442,8 @@ pub enum IsbnError {
     InvalidChecksum,
     /// Failed to convert to ISBN10.
     InvalidConversion,
+    /// One or supplied more digits were too large.
+    DigitTooLarge,
 }
 
 impl fmt::Display for IsbnError {
@@ -461,6 +460,10 @@ impl fmt::Display for IsbnError {
             }
             IsbnError::InvalidChecksum => write!(f, "Failed to validate checksum."),
             IsbnError::InvalidConversion => write!(f, "Failed to convert to ISBN10."),
+            IsbnError::DigitTooLarge => write!(
+                f,
+                "A supplied digit was larger than 9, or the ISBN10 check digit was larger than 10."
+            ),
         }
     }
 }
@@ -471,23 +474,44 @@ impl From<ParseIntError> for IsbnError {
     }
 }
 
+impl From<CapacityError<u8>> for IsbnError {
+    fn from(_: CapacityError<u8>) -> Self {
+        IsbnError::InvalidLength
+    }
+}
+
 #[derive(Debug, Clone)]
 struct Parser {
     digits: ArrayVec<[u8; 13]>,
 }
 
 impl Parser {
-    pub fn new(s: &str) -> Parser {
-        let digits = s
-            .chars()
-            .filter_map(|c| match c {
-                '-' => None,
-                ' ' => None,
-                'X' => Some(10u8),
-                _ => Some(c.to_digit(10).unwrap_or(0) as u8),
-            })
-            .collect();
-        Parser { digits }
+    pub fn new<S: AsRef<str>>(s: S) -> Result<Parser, IsbnError> {
+        let mut digits = ArrayVec::new();
+        let mut has_x = false;
+
+        for c in s.as_ref().chars() {
+            match c {
+                '-' | ' ' => {}
+                'X' => {
+                    if digits.len() == 9 {
+                        has_x = true;
+                        digits.push(10);
+                    } else {
+                        return Err(IsbnError::InvalidDigit);
+                    }
+                }
+                '0'..='9' => {
+                    if has_x {
+                        return Err(IsbnError::InvalidDigit);
+                    } else {
+                        digits.try_push(c.to_digit(10).unwrap() as u8)?
+                    }
+                }
+                _ => return Err(IsbnError::InvalidDigit),
+            }
+        }
+        Ok(Parser { digits })
     }
 
     fn read_isbn(&mut self) -> Result<Isbn, IsbnError> {
@@ -499,22 +523,22 @@ impl Parser {
     }
 
     fn read_isbn13(&mut self) -> Result<Isbn13, IsbnError> {
-        let check_digit = Isbn13::calculate_check_digit(&self.digits);
-        if check_digit == *self.digits.last().unwrap() {
-            let d = &self.digits;
-            Isbn13::new(
-                d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10], d[11], d[12],
-            )
+        let mut a = [0u8; 13];
+        a.clone_from_slice(&self.digits);
+        let check_digit = Isbn13::calculate_check_digit(&a);
+        if check_digit == a[12] {
+            Ok(Isbn13 { digits: a })
         } else {
             Err(IsbnError::InvalidDigit)
         }
     }
 
     fn read_isbn10(&mut self) -> Result<Isbn10, IsbnError> {
-        let check_digit = Isbn10::calculate_check_digit(&self.digits);
-        if check_digit == *self.digits.last().unwrap() {
-            let d = &self.digits;
-            Isbn10::new(d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9])
+        let mut a = [0; 10];
+        a.clone_from_slice(&self.digits);
+        let check_digit = Isbn10::calculate_check_digit(&a);
+        if check_digit == a[9] {
+            Ok(Isbn10 { digits: a })
         } else {
             Err(IsbnError::InvalidDigit)
         }
@@ -531,10 +555,11 @@ mod tests {
         assert!(Isbn::from_str("0-306-40615-2").is_ok());
 
         // Wikipedia ISBN-10 check digit calculation invalid example
-        assert!(Isbn::from_str("99999-999-9-X").is_err());
+        // assert!(Isbn::from_str("99999-999-9-X").is_err());
 
         // Wikipedia Registrant element examples
         assert!(Isbn::from_str("99921-58-10-7").is_ok());
+        assert!(Parser::new("9971-5-0210-0").is_ok());
         assert!(Isbn::from_str("9971-5-0210-0").is_ok());
         assert!(Isbn::from_str("960-425-059-0").is_ok());
         assert!(Isbn::from_str("80-902734-1-6").is_ok());
@@ -554,5 +579,24 @@ mod tests {
 
         // Wikipedia ISBN-13 check digit calculation example
         assert!(Isbn13::from_str("978-0-306-40615-7").is_ok());
+    }
+
+    #[test]
+    fn test_invalid_isbn_strings_no_panic() {
+        assert!(Isbn::from_str("L").is_err());
+        assert!(Isbn::from_str("").is_err());
+        assert!(Isbn::from_str("01234567890123456789").is_err());
+        assert!(Isbn::from_str("ⱧňᚥɂᛢĞžᚪ©ᛟƚ¶G").is_err());
+
+        assert!(Isbn10::from_str("").is_err());
+        assert!(Isbn10::from_str("01234567890").is_err());
+        assert!(Isbn10::from_str("01234567X9").is_err());
+        assert!(Isbn10::from_str("012345678").is_err());
+
+        assert!(Isbn13::from_str("").is_err());
+        assert!(Isbn13::from_str("012345678901X").is_err());
+        assert!(Isbn13::from_str("01234567890X2").is_err());
+        assert!(Isbn13::from_str("012345678").is_err());
+        assert!(Isbn13::from_str("0123456789012345").is_err());
     }
 }
